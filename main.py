88ae98afc58d3555102b39bbac3505881fe1ab7d
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2023-07-29 16:28:52 -0700

    Replace print with log, alter terminal instantiation process

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from zed_bot import terminal
 import discord
 from datetime import datetime
-from zed_bot import enums
+from zed_bot import terminal, enums, logger
+
+log = logger.get_logger()
 
 
 token = enums.TOKEN
@@ -16,19 +17,34 @@ bot = discord.Bot(intents=intents, debug_guilds=[server_id])
 async def on_ready():
     now = datetime.now()
     now = now.strftime("%d/%m/%Y %H:%M:%S")
-    print(f"Running {bot.user}. \nStarted at {now}.\nPing {bot.latency:3f}ms.")
+    log.info(
+        f"Running {bot.user}. \nStarted at {now}.\nPing {bot.latency:3f}ms."
+    )
 
 
 @bot.slash_command(name="start", description="start new terminal")
 async def start(ctx: discord.ApplicationContext):
-    await terminal.create_terminal(ctx.author, ctx=ctx)
+    if not ctx.author.bot:
+        # name = f"{ctx.author.id}"
+        cat = discord.utils.get(ctx.guild.categories, name="Terminals")
+        channel = await ctx.guild.create_text_channel(ctx.author.id, category=cat)
+        global active_terminal
+        active_terminal = terminal.Terminal(user=ctx.author, channel=channel)
+        await channel.set_permissions(ctx.author, read_messages=True, send_messages=True)
+        await channel.send(
+            f"```>\n>\n>\n>\n>\n>\n>\n>\n>\n>\n>\n>\n>STARTED NEW TERMINAL FOR {ctx.author.name.upper()} USE 'help' FOR COMMAND LIST```"
+        )
+    else:
+        await ctx.send("You can't invoke this command, you nasty bot >:p")
 
 
 @bot.event
 async def on_message(msg: discord.Message):
-    if not msg.author.bot:
-        if str(msg.channel.category) == "Terminals":
-            terminal.on_command(msg)
+    if "active_terminal" in globals():
+        if msg.author.name == active_terminal.user.name:
+            active_terminal.on_command(msg)
+    else:
+        log.warning("No active terminal found, aborting command")
 
 
 bot.run(token)
--- zed_bot/terminal.py
+++ zed_bot/terminal.py
@@ -1,65 +1,58 @@
 import time
 import discord
+from zed_bot import logger
 
-active_terminals = []
-
-
-async def create_terminal(
-    user: discord.Member, ctx: discord.ApplicationContext
-):
-    name = f"{user.id}"
-    cat = discord.utils.get(ctx.guild.categories, name="Terminals")
-    channel = await ctx.guild.create_text_channel(name, category=cat)
-    new_terminal = Terminal(user, channel)
-    active_terminals.append(new_terminal)
-    await channel.set_permissions(user, read_messages=True, send_messages=True)
-    await channel.send(
-        f"```>\n>\n>\n>\n>\n>\n>\n>\n>\n>\n>\n>\n>STARTED NEW TERMINAL FOR {user.name.upper()} USE 'help' FOR COMMAND LIST```"
-    )
-
-
-def check_if_command(cmd: str):
-    is_command = False
-    with open("commands.txt", "r") as f:
-        command_list = f.readlines()
-        for command in command_list:
-            if cmd.startswith(command):
-                is_command = True
-    return is_command
-
-
-def on_command(msg: discord.Message):
-    for term in active_terminals:
-        try:
-            print(str(term.channel_id), ",", str(msg.channel.id))
-            if str(term.channel_id) == str(msg.channel.id):
-                try:
-                    term.command(msg=msg)
-                except:
-                    print("Failed to exicute command")
-        except:
-            print("Failed to find terminal")
+log = logger.get_logger()
 
 
 class Terminal:
     def __init__(self, user: discord.Member, channel: discord.TextChannel):
         self.user = user
-        self.channel_id = channel.id
+        self.channel = channel
         self.creation_date = time.time()
-        self.lastUseTime = self.creation_date
+        self.last_use_time = self.creation_date
         self.log = []
         self.command_log = []
 
     def lifespan_check(self):
-        if self.lastUseTime > time.time() + 120:
+        if self.last_use_time > time.time() + 120:
             return True
         else:
             return False
 
-    def command(msg: discord.Message):
-        print("Got command")
+    def command(self, msg: discord.Message):
+        log.info(f"Got command: {msg.content}")
         user = msg.author
         cmd = msg.content.lower()
-        if check_if_command(cmd):
-            if cmd.startswith("help"):
-                print(f'User{user} used help cmd "{cmd}"')
+        if self.check_if_command(cmd):
+            log.info(f'User {user.name} used help cmd "{cmd}"')
+
+    def check_if_command(self, cmd: str):
+        is_command = False
+        with open("./zed_bot/commands.txt", "r") as f:
+            command_list = f.readlines()
+            for command in command_list:
+                if cmd.startswith(command):
+                    is_command = True
+        return is_command
+
+    def on_command(self, msg: discord.Message):
+        try:
+            log.info(
+                f"Channel: {str(self.channel.id)}, Message: {str(msg.channel.id)}"
+            )
+            try:
+                if not (
+                    self.channel.id == msg.channel.id or msg.author == self.user
+                ):
+                    log.error(
+                        "Unable to execute command, channel or author discrepancy"
+                    )
+                    raise Exception("channel or author discrepancy")
+                else:
+                    self.command(msg=msg)
+            except Exception as e:
+                log.exception(f"Unknown exception: {e}")
+                raise e
+        except Exception as e:
+            log.exception(f"Failed to find terminal: {e}")
