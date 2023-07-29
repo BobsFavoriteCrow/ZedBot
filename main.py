58c9b23b96016b59f71359c6e030785282bc3117
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2023-07-29 15:24:30 -0700

    Merge zed's main

--- main.py
+++ main.py
@@ -1,79 +1,34 @@
-"""Main module for calling zedBot."""
-from random import randint
-
+from zed_bot import terminal
 import discord
-
-from zed_bot import bot as my_bot
-
-from zed_bot import logger, enums
-
-# Create logger
-log = logger.get_logger()
-
-# Instantiate ZedBot
-bot = my_bot.ZedBot()
+from datetime import datetime
+from zed_bot import enums
 
 
-# Create slash commands
-@bot.slash_command(
-    name="hello",
-    description="Say hello to the bot",
-    guild_ids=[enums.GUILD_ID],
-)
-async def hello(ctx: discord.ApplicationContext):
-    log.info("We received a slash command!")
-    await ctx.respond("Hello!")
+token = enums.TOKEN
+server_id = enums.GUILD_ID
+
+intents = discord.Intents.all()
+
+bot = discord.Bot(intents=intents, debug_guilds=[server_id])
 
 
-@bot.slash_command(
-    name="ping",
-    description="Check the latency of the bot",
-    guild_ids=[enums.GUILD_ID],
-)
-async def ping(ctx: discord.ApplicationCommand):
-    bot_latency = int(float(f"{bot.latency:.3f}")*1000)
-    await ctx.respond(f"Pong! Latency is {bot_latency:3}ms.")
+@bot.event
+async def on_ready():
+    now = datetime.now()
+    now = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(f"Running {bot.user}. \nStarted at {now}.\nPing {bot.latency:3f}ms.")
 
 
-@bot.slash_command(
-    name="guess_the_number",
-    description="Guess the number between 1 and 10",
-    guild_ids=[enums.GUILD_ID],
-)
-async def gtn(ctx: discord.ApplicationCommand):
-    """Play this stupid game and guess a number.
-
-    Parameters
-    ----------
-    ctx : discord.ApplicationCommand
-        _description_
-    """
-    answer: int = randint(1, 10)
-    log.info(f"The answer is: {answer}")
-    await ctx.respond("Guess a number between 1 and 10 within 30 seconds.")
-
-    win = False
-    timeout = 30
-    while not win:
-        guess: discord.Message = await bot.wait_for(
-            "message",
-            check=lambda message: message.author == ctx.author,
-            timeout=timeout,
-        )
-        log.info(f"User guess content: {guess.content}")
-
-        if guess.content == "":
-            await ctx.send(
-                "Please respond directly to my messages for me to read your guess!"
-            )
-        elif int(float(guess.content)) == answer:
-            await ctx.send("You guessed it!")
-            win = True
-            break
-        else:
-            await ctx.send("Nope, try again.")
-        continue
+@bot.slash_command(name="start", description="start new terminal")
+async def start(ctx: discord.ApplicationContext):
+    await terminal.create_terminal(ctx.author, ctx=ctx)
 
 
-# Run bot
-bot.run(enums.TOKEN)
+@bot.event
+async def on_message(msg: discord.Message):
+    if not msg.author.bot:
+        if str(msg.channel.category) == "Terminals":
+            terminal.on_command(msg)
+
+
+bot.run(token)
